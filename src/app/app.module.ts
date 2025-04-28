8a7428e70f99c4e2d67b287a70a40ac2550e5d4c
Sylvia Chebet <sylviachebet03@gmail.com>
2025-04-28 11:20:22 +0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './pages/task-list/task-list.component';
+import { TaskFormComponent } from './pages/task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
